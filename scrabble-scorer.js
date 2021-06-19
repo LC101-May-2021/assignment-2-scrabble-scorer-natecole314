9fb6558afc9581e9ed88a24180e10715f5553abd
natecole314 <84357074+natecole314@users.noreply.github.com>
2021-06-19 03:05:28 +0000

    check failures

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -29,13 +29,18 @@ function oldScrabbleScorer(word) {
 	return letterPoints;
  }
 
-console.log(oldScrabbleScorer("tape"))
+
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+  console.log("Let's play some scrabble!");
+   console.log();
+   let userInput = input.question("Enter a word to score: ")
+   return userInput
 };
+let userOutput = initialPrompt();
+console.log(userOutput);
 
 let simpleScore;
 
@@ -55,6 +60,7 @@ function runProgram() {
    initialPrompt();
    
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
