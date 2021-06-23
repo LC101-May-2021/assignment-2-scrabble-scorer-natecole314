b430cdee7a124a935e94d74805e4dd48d2354491
natecole314 <84357074+natecole314@users.noreply.github.com>
2021-06-23 03:48:35 +0000

    newPointStructure added

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -83,21 +83,46 @@ let vowelBonusScore = function(word){
 	}
 	return letterPoints;
 };
-
+/*{
+  A: '1',
+  E: '1',
+  I: '1',
+  O: '1',
+  U: '1',
+  L: '1',
+  N: '1',
+  R: '1',
+  S: '1',
+  T: '1',
+  D: '2',
+  G: '2',
+  B: '3',
+  C: '3',
+  M: '3',
+  P: '3',
+  F: '4',
+  H: '4',
+  V: '4',
+  W: '4',
+  Y: '4',
+  K: '5',
+  J: '8',
+  X: '8',
+  Q: '10',
+  Z: '10'
+} */
 let scrabbleScore = function (word) {
 	word = word.toUpperCase();
 	let letterPoints = 0;
  
 	for (let i = 0; i < word.length; i++) {
+    letterPoints += Number(newPointStructure[word[i]])
+	  //for (key in newPointStructure) {
  
-	  for (const pointValue in oldPointStructure) {
- 
-		 if (oldPointStructure[pointValue].includes(word[i])) {
+		 //if (newPointStructure[key].includes(word[i])) {
 			//letterPoints += `Points for '${word[i]}': ${pointValue}\n`
-      letterPoints += Number(pointValue);
-		 }
+     // letterPoints += Number(key[i]);
  
-	  }
 	}
 	return letterPoints;
 };
@@ -123,16 +148,17 @@ function scorerPrompt() {
 
 }
 function transform(object) {
+let newObject = {}
 for (item in object){
-  let newObject = {}
   //console.log(item)
   //console.log(oldPointStructure[item])
   for (i = 0; i < object[item].length; i++){
+   
     //console.log(oldPointStructure[item][i])
     newObject[object[item][i]] = item
     
   } 
-}
+}return newObject;
 }
 
 //console.log(oldPointStructure[1].length);
@@ -157,7 +183,7 @@ for (item in object){
  // };
 
 let newPointStructure = transform(oldPointStructure);
-
+console.log(newPointStructure)
 function runProgram() {
    let promptOutput = initialPrompt();
    let scorerPromptOutput = scorerPrompt();
